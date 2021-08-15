25c8764a2de839fa8ba6d662f1e0eaddc281ae07
metzkorn <etzkornmichael@gmail.com>
2021-08-15 15:06:23 -0500

    last look for the day

--- 2020/kitsune/rc6/kitsune20.js
+++ 2020/kitsune/rc6/kitsune20.js
@@ -38994,7 +38994,7 @@ var Bq = function(b, g, m) {
         k = b.localToLocal(0, 0, m);
         m = b.localToLocal(c.x, c.y, m);
         g = g.ec.get(aj);
-        g = B(m.x - k.x, m.y - k.y - (g ? .6 * g.z * 10000 : 0));
+        g = B(m.x - k.x, m.y - k.y - (g ? .6 * g.z * 3 : 0)); // originally g? .6 * g.z * 3 : 0
         .1 > Math.abs(g.x) && (g.x = 0);
         .1 > Math.abs(g.y) && (g.y = 0);
         return B(b.x - g.x, b.y - g.y)
@@ -39026,6 +39026,7 @@ Dq.prototype.tick = function() {
     (m = Math.min(g.Aab*m, g.speed) / m) && g.Baa && (m = gh(zj(b), m, g.Qw), Aj(b, m));
     m = b.globalToLocal(0, 0);
     b = b.globalToLocal(3840, 2160); // Originally 960, 540
+    b = new createjs.Rectangle(g.Qw.x/(-3), g.Qw.y/(-3), b.x - m.x, b.y - m.y)
     b = new createjs.Rectangle(m.x, m.y, b.x - m.x, b.y - m.y);
    // b = new createjs.Rectangle(0, 0, 10000, 10000); // but what if this makes it so you can see the whole map // This didnt do anything
     g.viewport = b
@@ -39488,7 +39489,7 @@ gr.prototype.tick = function() {
             h = Math.round(8 * C(n)) / 8,
             d = mh(n),
             roll_speed = 20, 
-            walk_speed = 15; // this gets modified by h. I don't remember what C does. TODO: Look at what C does. 
+            walk_speed = 10000; // this gets modified by h. I don't remember what C does. TODO: Look at what C does. 
             ;
            // console.log("gr function triggered: k.velocity is:" + k.velocity)
         0 < m.Jaa ? m.Jaa-- : 0 < C(n) ? a.Ca[4] ? (Nj(g, "roll", b.ha), m.Jaa = 9, k.velocity = lg(n, roll_speed), c.direction = d, A.uja.play()) : (Nj(g, "walk", b.ha), k.velocity = lg(n, walk_speed * h), c.direction = d) : (Nj(g, "idle", b.ha), k.velocity = B(0, 0))
